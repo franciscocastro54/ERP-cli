a2017f114a78cdec56cd926d534101ceb6fe000e
camilo <camiloinuneko@gmail.com>
2021-05-23 17:22:26 -0400

    Modulos_control actualizacion

--- src/main/java/Modulos_control.java
+++ src/main/java/Modulos_control.java
@@ -33,7 +33,15 @@ public class Modulos_control extends HttpServlet {
             throws ServletException, IOException {
    response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
+<<<<<<< HEAD
             /* TODO output your page here. You may use following sample code. */
+=======
+           String modulo= request.getParameter("m").toString();
+           String opcion="";
+           if(request.getParameter("op")!=null)opcion = request.getParameter("op").toString();
+            /* TODO output your page here. You may use following sample code. */
+            
+>>>>>>> d5e2d42386c67a4bd69b2ba4825561c9ced513f7
             out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">");
             out.println("<html>");
             out.println("<head>");
@@ -48,11 +56,53 @@ public class Modulos_control extends HttpServlet {
             out.println("</div>");
             out.println("<div class=\"row\">");
             out.println("<div class=\"col-2\">");
+<<<<<<< HEAD
             request.getRequestDispatcher("/componentes/menu_mod.jsp").include(request, response);
             out.println("</div>");
             out.println("<div class=\"col-10\">");
             //aqui abajo va el contenido
             request.getRequestDispatcher("/Clientes/Feedback.jsp").include(request, response);
+=======
+            switch(modulo){
+            case "cli":
+            request.getRequestDispatcher("/componentes/menu_mod.jsp").include(request, response);
+            break;
+            
+            default:
+                //aqui va error404
+                out.println("<h2> ERROR DE MODULO</h2>");
+           break;
+            }
+                    out.println("</div>");
+            out.println("<div class=\"col-10\">");
+            //aqui abajo va el contenido  
+            switch(opcion){
+                case "feed":
+            request.getRequestDispatcher("/Clientes/Feedback.jsp").include(request, response);
+                 break;
+                case "His":
+                    request.getRequestDispatcher("/Clientes/Historial_clientes.jsp").include(request, response);
+                    break;
+                    case "modcli":
+                    request.getRequestDispatcher("/Clientes/ModificarCliente.jsp").include(request, response);
+                    break;
+                    case "modemp":
+                    request.getRequestDispatcher("/Clientes/ModificarEmpresa.jsp").include(request, response);
+                    break;
+                    case "regcli":
+                    request.getRequestDispatcher("/Clientes/RegistrarCliente.jsp").include(request, response);
+                    break;
+                    case "regemp":
+                    request.getRequestDispatcher("/Clientes/RegistrarEmpresa.jsp").include(request, response);
+                    break;
+                    case "ord":
+                    request.getRequestDispatcher("/Clientes/ordenServicio.jsp").include(request, response);
+                    break;
+                default:
+                   //aqui va pagina por defecto
+                    break;
+            }
+>>>>>>> d5e2d42386c67a4bd69b2ba4825561c9ced513f7
             out.println("</div>");
             out.println("</div>");
             out.println("</div>");
